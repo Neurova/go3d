1f4fb706aabf3784dace9f633d01eed7b433c5f6
JBuffa10 <50505391+JBuffa10@users.noreply.github.com>
2022-08-28 17:56:28 -0500

    J buffa10/issue2 (#7)
    * geom package
    Fixes #2
    
    * Normalize function passes test
    
    * delete commented out code
    
    Co-authored-by: Caleb Buffa <62570757+calebbuffa@users.noreply.github.com>

--- geom/geom.go
+++ geom/geom.go
@@ -26,7 +26,7 @@ func magnitude(m *mat.Dense) []float64 {
 
 	if r == 1 {
 
-		v := m.RowView(r)
+		var v mat.Vector = m.RowView(r)
 
 		dist[0] = mat.Norm(v, 2) // 2 = euclidean norm
 
@@ -34,10 +34,62 @@ func magnitude(m *mat.Dense) []float64 {
 
 		for i := 0; i <= r-1; i++ {
 
-			v := m.RowView(i)
+			var v mat.Vector = m.RowView(i)
 
 			dist[i] = mat.Norm(v, 2) // 2 = euclidean norm
 		}
 	}
 	return dist
+}
+
+// normalize function applies rowwise normalization of 2D or 3D data
+// to magnatiude 1
+func normalize(m *mat.Dense) *mat.Dense {
+
+	if m.IsEmpty() {
+		panic(mat.ErrZeroLength)
+	}
+
+	r, c := m.Dims()
+
+	var index = make([]float64, c)
+
+	// return variable with the same shape as m
+	norm := mat.NewDense(r, c, nil)
+
+	var mag []float64 = magnitude(m)
+
+	if r == 1 {
+
+		var row []float64 = m.RawMatrix().Data
+
+		var normRow = make([]float64, c)
+
+		for idx, value := range row {
+
+			normRow[idx] = value / mag[0] // if only 1 row, then mag only has a zero index
+
+		}
+
+		norm.SetRow(0, normRow)
+
+	} else {
+
+		for i := 0; i <= r-1; i++ {
+
+			var normRow = make([]float64, c)
+
+			var row []float64 = mat.Row(index, i, m)
+
+			for idx, value := range row {
+
+				normRow[idx] = value / mag[i]
+
+			}
+
+			norm.SetRow(i, normRow)
+		}
+	}
+
+	return norm
 }
