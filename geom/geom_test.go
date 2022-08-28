5d6cb77166976b5fc5d44d461b57a52ae19cc8a3
Jacob <jacobbuffa10@gmail.com>
2022-08-28 17:38:35 -0500

    Normalize function passes test

--- geom/geom_test.go
+++ geom/geom_test.go
@@ -1,6 +1,7 @@
 package geom
 
 import (
+	"fmt"
 	"testing"
 
 	"gonum.org/v1/gonum/mat"
@@ -41,3 +42,57 @@ func TestMagnitude(t *testing.T) {
 	}
 
 }
+
+func TestNormalize(t *testing.T) {
+	t.Parallel()
+
+	const proofMagnitude float64 = 1
+	const cols int = 3
+	const rows int = 2
+
+	x := []float64{1, 2}
+	y := []float64{3, 4}
+	z := []float64{5, 6}
+
+	a := mat.NewDense(rows, cols, nil)
+	a.SetCol(0, x)
+	a.SetCol(1, y)
+	a.SetCol(2, z)
+
+	normA := normalize(a)
+
+	fmt.Println("A", a)
+	fmt.Println("Magnitude A", magnitude(a))
+	fmt.Println("Normalized A", normA)
+
+	var rowOneMag float64
+	var rowTwoMag float64
+
+	var rowOne mat.Vector = normA.RowView(0)
+	var rowTwo mat.Vector = normA.RowView(1)
+
+	rowOneMag = math.Sqrt(rowOne.AtVec(0)*rowOne.AtVec(0) + rowOne.AtVec(1)*rowOne.AtVec(1) + rowOne.AtVec(2)*rowOne.AtVec(2))
+	rowTwoMag = math.Sqrt(rowTwo.AtVec(0)*rowTwo.AtVec(0) + rowTwo.AtVec(1)*rowTwo.AtVec(1) + rowTwo.AtVec(2)*rowTwo.AtVec(2))
+
+	r, c := normA.Dims()
+
+	if r != rows {
+		t.Errorf("undexpected number of columns:  got %d  wanted: %d", c, cols)
+	}
+
+	if c != cols {
+		t.Errorf("undexpected number of rows:  got %d  wanted: %d", r, rows)
+	}
+
+	if math.Round(rowOneMag) != proofMagnitude {
+		t.Errorf("unexpected magnitude for row one:  got %f  wanted: 1", rowOneMag)
+	}
+
+	if math.Round(rowTwoMag) != proofMagnitude {
+		t.Errorf("unexpected magnitude for row two:  got %f  wanted: 1", rowTwoMag)
+	}
+
+}
+
+// 5.91 row 1 mangnitude
+// .169 + .507 +
